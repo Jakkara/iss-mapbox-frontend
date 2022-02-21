60cf01402be206a36035e1623ee3835c29c347a0
Jali Rainio <jali.rainio@sofokus.com>
2022-02-21 22:05:59 +0200

    Add basic fetch operation for coordinates
    Fetch is repeated automatically at intervals

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const API_URL = 'http://api.open-notify.org/iss-now.json'
+const SUCCESS_STATUS = 'success'
+
+type Location = {
+  latitude: string
+  longitude: string
+}
+
+type APIResponse = {
+  message: string
+  timestamp: string
+  iss_position: Location
+}
 
 const MapPage = () => {
-  return (
-    <div>
-      Test
-    </div>
-  )
+  const [activeCoordinates, setActiveCoordinates] = useState<Location | null>()
+
+  useEffect(() => {
+    const refreshCoordinates = async () => {
+      const {
+        data: { message, iss_position }
+      } = await axios.get<APIResponse>(API_URL as string)
+      if (message !== SUCCESS_STATUS) {
+        return
+      }
+      setActiveCoordinates(iss_position)
+    }
+    refreshCoordinates()
+    setInterval(refreshCoordinates, 15000)
+  }, [])
+
+  return <div>{JSON.stringify(activeCoordinates)}</div>
 }
 
 export default MapPage
